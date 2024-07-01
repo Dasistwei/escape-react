95111bfaa6dce86a8436d552190534adb3819011
wei <wei02919@gmail.com>
2024-07-01 18:19:14 +0800

    add article/article details page

--- src/pages/ArticleDetail.jsx
+++ src/pages/ArticleDetail.jsx
@@ -1,47 +1,107 @@
-<div className="container">
-  <div className="row">
-    <div className="col-md-6">
-      <div className="card">
-        <div className="card-header bg-white">
-          <h5 className="card-title">文章內容</h5>
-        </div>
-        <div className="card-body">
-          <form>
-            <div className="mb-3">
-              <label htmlFor="title" className="form-label">文章名稱</label>
-              <input type="text" className="form-control" id="title" />
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+export const ArticleDetail = () => {
+  const { articleId } = useParams();
+  const [article, setArticle] = useState([]);
+
+  const fetchArticle = () => {
+    axios
+      .get(`${process.env.REACT_APP_URL}api/react/article/${articleId}`)
+      .then((res) => setArticle(res.data.article))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    fetchArticle();
+  }, []);
+  // console.log("articleId", article);
+  //   {
+  //   "author": "escape",
+  //   "content": "這是內容",
+  //   "create_at": 1555459220,
+  //   "description": "文章內容",
+  //   "id": "-MwQZ1P-8ur-z_jIAPCA",
+  //   "image": "test.testtest",
+  //   "isPublic": true,
+  //   "tag": [
+  //     "tag1"
+  //   ],
+  //   "title": "新增第一篇文章"
+  // }
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header bg-white">
+              <h5 className="card-title">文章內容</h5>
             </div>
-            <div className="mb-3">
-              <label htmlFor="author" className="form-label">文章作者</label>
-              <input type="text" className="form-control" id="author" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="content" className="form-label">文章描述</label>
-              <textarea className="form-control" id="content" />
-            </div>
-            <div className="row gx-1 mb-3">
-              <div className="col-md-2 mb-1">
-                <div className="input-group input-group-sm">
-                  <input type="text" className="form-control form-control" id="tag" placeholder="請輸入標籤" />
-                  <button type="button" className="btn btn-outline-danger">x</button>
+            <div className="card-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">
+                    文章名稱
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    value={article.content}
+                  />
                 </div>
-              </div>
+                <div className="mb-3">
+                  <label htmlFor="author" className="form-label">
+                    文章作者
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="author"
+                    value={article.author}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="content" className="form-label">
+                    文章描述
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="content"
+                    value={article.description}
+                  />
+                </div>
+                <div className="row gx-1 mb-3">
+                  <div className="col-md-2 mb-1">
+                    <div className="input-group input-group-sm">
+                      <input
+                        type="text"
+                        className="form-control form-control"
+                        id="tag"
+                        placeholder="請輸入標籤"
+                        value={article.tag}
+                      />
+                      <button type="button" className="btn btn-outline-danger">
+                        x
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header bg-white">
+              <h5 className="card-title">文章圖片</h5>
+            </div>
+            <div className="card-body">
+              <img src={article.image} alt="article-logo" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div className="col-md-6">
-      <div className="card">
-        <div className="card-header bg-white">
-          <h5 className="card-title">文章圖片</h5>
-        </div>
-        <div className="card-body">
-          ...
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-</div>
+  );
+};
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,18 +1,19 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Product() {
   const [products, setProducts] = useState([]);
   const getData = () => {
-    axios.get(`${process.env.REACT_APP_URL}api/react/products`)
+    axios
+      .get(`${process.env.REACT_APP_URL}api/react/products`)
       .then((response) => {
         setProducts(response.data.products);
       });
-  }
+  };
   useEffect(() => {
     getData();
-  }, [])
+  }, []);
   return (
     <div className="container">
       <div className="card">
@@ -32,31 +33,33 @@ function Product() {
               </tr>
             </thead>
             <tbody>
-
-              {
-                products.map((item, index) => {
-                  return <tr key={index}>
+              {products.map((item, index) => {
+                return (
+                  <tr key={index}>
                     <th scope="row">{index + 1}</th>
                     <td>{item.title}</td>
                     <td>{item.category}</td>
-                    <td>{item.is_enabled ? '供應中' : ''}</td>
+                    <td>{item.is_enabled ? "供應中" : ""}</td>
                     <td>{item.price}</td>
                     <td>
-                      <Link to={item.id} className="btn btn-sm btn-neutral">查看</Link>
+                      <Link to={item.id} className="btn btn-sm btn-neutral">
+                        查看
+                      </Link>
                     </td>
                   </tr>
-                })
-              }
-
+                );
+              })}
             </tbody>
           </table>
         </div>
         <div className="card-footer bg-white py-3 text-end">
-          <span className="text-muted text-sm ">總共{products.length}筆資料</span>
+          <span className="text-muted text-sm ">
+            總共{products.length}筆資料
+          </span>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Product;
